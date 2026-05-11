0ccf053f16371bc134af896e570416797d850f89
tamachano <i.cclimber.luigi@gmail.com>
2026-05-11 10:51:25 +0900

    fix import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import NiceBuy from "../NiceBuy.jsx";
+import NiceBuy from "./NiceBuy.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
